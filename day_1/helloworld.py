2c2480b9f8489fa0bcc33f2459c1513754e0d0ac
gxctq56 <paradism147147@gmail.com>
2026-05-06 14:20:34 -0400

    Update helloworld.py

--- day_1/helloworld.py
+++ day_1/helloworld.py
@@ -1,1 +1,21 @@
+python3 --version
+3+4
+3-4
+3*4
+3%4
+3/4
+3**4
+3//4
+"Name"
+"Family Name"
+"Canada"
+
+type(10)
+type(9.8)
+type(3.14)
+type(4-4j)
+type(['Asabeneh', 'Python', 'Finland'])
+type("Name")
+type("Family Name")
+type("Canada")
 
